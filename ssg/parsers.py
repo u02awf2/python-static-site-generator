90738311b8f205835c3a6881ae7d818071721059
Alan Fraser <afraser6@slb.com>
2021-08-09 16:31:18 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -17,3 +17,5 @@ class Parser:
 
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(content)
