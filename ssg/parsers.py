4efbd6186d9654f21c936477ddb9c3661de4011e
Alan Fraser <afraser6@slb.com>
2021-08-09 15:07:06 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,3 +7,6 @@ class Parser:
 
     def valid_extension(self, extension):
         return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
