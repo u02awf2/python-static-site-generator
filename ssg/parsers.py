bfd2e4cc7f770929cbfcb0d7f872a3dec93ab6b3
Alan Fraser <afraser6@slb.com>
2021-08-09 16:14:47 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -10,3 +10,7 @@ class Parser:
 
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
+
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
