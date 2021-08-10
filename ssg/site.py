37c8d566e0dda9930686296e22aa12573b12f657
Alan Fraser <afraser6@slb.com>
2021-08-10 09:14:47 +0100

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -28,5 +28,7 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-
+            elif path.is_file():
+                self.run_parser(path)
+                
 
