532985a1abe5f967f5baa8578c9b74835088b4a8
Alan Fraser <afraser6@slb.com>
2021-08-10 09:10:27 +0100

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -17,6 +17,10 @@ class Site:
 
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
 
 
     def build(self):
