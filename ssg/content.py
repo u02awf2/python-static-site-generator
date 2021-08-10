9d0549f254e5218fffc41ca562012adf87f33d72
Alan Fraser <afraser6@slb.com>
2021-08-10 09:42:53 +0100

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -8,3 +8,8 @@ class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
