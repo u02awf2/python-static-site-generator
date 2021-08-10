86d251a7c59d157e87b4a3b0018274068e4972af
Alan Fraser <afraser6@slb.com>
2021-08-10 09:52:23 +0100

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -13,3 +13,7 @@ class Content(Mapping):
         _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data ["content"] = content
