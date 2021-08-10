33b61b9c5aa236091052b0a43669caa7bca49c4d
Alan Fraser <afraser6@slb.com>
2021-08-10 09:29:20 +0100

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -1,5 +1,10 @@
 import re
 
-from yaml import (load, FullLoader)
-
 from collections.abc import Mapping
+from yaml import load, FullLoader
+
+
+class Content(Mapping):
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
