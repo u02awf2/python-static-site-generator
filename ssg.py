3892a4a6ddca4d1f1622a960cd220ceb7e2536e1
Alan Fraser <afraser6@slb.com>
2021-08-11 10:30:29 +0100

    Completed module

--- ssg.py
+++ ssg.py
@@ -9,7 +9,10 @@ def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
-        "parsers": [ssg.parsers.ResourceParser(), ],
+        "parsers": [ssg.parsers.ResourceParser(),
+                    ssg.parsers.MarkdownParser(),
+                    ssg.parsers.ReStructuredTextParser(),
+                    ],
     }
 
     Site(**config).build()
