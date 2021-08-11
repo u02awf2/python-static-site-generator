76553bff0d4e7bf66a357ccbcd390635760eb1ef
Alan Fraser <afraser6@slb.com>
2021-08-11 09:22:09 +0100

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -32,3 +32,9 @@ class Content(Mapping):
 
     def __getitem__(self, key):
         return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
