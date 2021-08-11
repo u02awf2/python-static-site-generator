eff576a3244405c4c059f689784d66750dd77866
Alan Fraser <afraser6@slb.com>
2021-08-11 09:31:26 +0100

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -41,4 +41,7 @@ class Content(Mapping):
 
     def __repr__(self):
         data = {}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
         return str(data)
